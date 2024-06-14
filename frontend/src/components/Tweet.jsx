5a144b9edf4d5a5ed4b0f6f0c1480d21520d0b3d
17sTomy <tomasignacioalv@hotmail.com>
2024-06-14 15:56:39 -0300

    fix bug for deleting tweets

--- frontend/src/components/Tweet.jsx
+++ frontend/src/components/Tweet.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Retweet from "../components/Retweet";
 import Like from "../components/Like";
  
-const Tweet = ({ t, deleteTweetMutation }) => {
+const Tweet = ({ t }) => {
   return (
     <div key={t.id} className="border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-900 transition">
       <div className="flex flex-row items-start gap-3">
@@ -42,7 +42,7 @@ const Tweet = ({ t, deleteTweetMutation }) => {
             <div className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500">
               <Like t={t} />
             </div>
-            {deleteTweetMutation && (
+            {t.user === localStorage.getItem('username') && (
               <div className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500">
                 <BsFillTrashFill onClick={() => deleteTweetMutation.mutate(t.id)} size={17} />
               </div>
